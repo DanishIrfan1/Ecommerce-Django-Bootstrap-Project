e630b3097d195cf8469c33d3634a4cb11946991e
Danish Irfan <danishuoc@gmail.com>
2023-08-13 13:28:21 +0500

    13 August Cart and store functionalities completed here

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 
 
@@ -13,11 +14,25 @@ def _cart_id(request):  # _cart_id() is used to get the cart_id from the session
     return cart
 
 
-def add_cart(request, product_id):
-    color = request.POST['color']
-    size = request.POST['size']
-    
+def add_cart(request, product_id): # add_cart() is used to add the cart_item to the cart
     product = Product.objects.get(id=product_id)  # get the product
+    product_variation = []  # product_variation is used to store the variation of the product
+    if request.method == 'POST':
+        for item in request.POST: # iterate over the request.POST, because the request.POST contains the product_id and the variation_id, multiple variation_id can be present
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get( # get the variation using the product_id, key and value present in the request.POST
+                    product=product_id,
+                    variation_category__iexact=key,
+                    variation_value__iexact=value,
+                )
+                product_variation.append(variation) # append the variation in the product_variation
+            except:
+                pass
+
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))  # get the cart using the _cart_id present in the session
         # print("cart already exists")
@@ -27,38 +42,74 @@ def add_cart(request, product_id):
         )
         # print("cart created")
     cart.save()  # save the cart in the session
-    # print("cart session id: ", cart.cart_id)
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)  # get the cart_item using the product and cart
-        cart_item.quantity += 1  # if the cart_item already exists, then increase the quantity by 1
-        cart_item.save()
-    except CartItem.DoesNotExist:  # if the cart_item does not exist, then create the cart_item
+    # print
+
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()  # check if the cart_item already exists or not
+    if is_cart_item_exists:  # if the cart_item already exists, then increase the quantity by 1
+        # Create the cart item, if the cart item already exists, then increase the quantity also works for the product with variation
+        cart_item = CartItem.objects.filter(product=product,cart=cart)  # get the cart_item using the product and cart
+        # existing_variation -> database
+        # current_variation -> product_variation list
+        # item_id -> database
+
+        ex_var_list = []  # ex_var_list is used to store the variation of the cart_item
+        id = []  # id is used to store the id of the cart_item
+        for item in cart_item: # iterate over the cart_item
+            existing_variation = item.variation.all() # get all the variation of the cart_item
+            ex_var_list.append(list(existing_variation)) # append the variation in the ex_var_list
+            id.append(item.id) # append the id in the id list
+        print(ex_var_list)
+        if product_variation in ex_var_list: # if the product_variation is in the ex_var_list, then increase the quantity by 1
+            # increase the cart_item quantity by 1
+            index = ex_var_list.index(product_variation) # get the index of the product_variation
+            item_id = id[index] # get the id of the cart_item using the index
+            item = CartItem.objects.get(product=product, id=item_id) # get the cart_item using the product and id
+            item.quantity += 1 # increase the quantity by 1
+            item.save() # save the cart_item
+        else: # if the product_variation is not in the ex_var_list, then create the cart_item
+            item = CartItem.objects.create(
+                product=product,
+                quantity=1,
+                cart=cart,
+            )
+            if len(product_variation) > 0:  # if the product_variation is greater than 0, then the product has variation
+                item.variation.clear()  # clear the cart_item variation
+                item.variation.add(*product_variation) # add the variation in the cart_item, * mark is used to iterate over the product_variation
+        # cart_item.quantity += 1  # if the cart_item already exists, then increase the quantity by 1
+            item.save()
+    else:  # if the cart_item does not exist, then create the cart_item
         cart_item = CartItem.objects.create(
             product=product,
             quantity=1,
             cart=cart,
         )  # create the cart_item
+        if len(product_variation) > 0:  # if the product_variation is greater than 0, then the product has variation
+            cart_item.variation.clear()  # clear the cart_item variation
+            cart_item.variation.add(*product_variation) # add the variation in the cart_item, * mark is used to iterate over the product_variation
         cart_item.save()
     return redirect('cart')
 
 
-def remove_cart(request, product_id):  # remove_cart() is used to remove the cart_item from the cart for (-) button
+def remove_cart(request, product_id,cart_item_id):  # remove_cart() is used to remove the cart_item from the cart for (-) button
     cart = Cart.objects.get(cart_id=_cart_id(request))  # get the cart using the cart_id present in the session
     product = get_object_or_404(Product, id=product_id)  # get the product
-    cart_item = CartItem.objects.get(product=product, cart=cart)  # get the cart_item using the product and cart
-    if cart_item.quantity > 1:  # if the quantity of the cart_item is greater than 1
-        cart_item.quantity -= 1  # then decrease the quantity by 1
-        cart_item.save()  # save the cart_item
-    else:
-        cart_item.delete()  # else delete the cart_item
+    try: # try to get the cart_item
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id )  # get the cart_item using the product and cart
+        if cart_item.quantity > 1:  # if the quantity of the cart_item is greater than 1
+            cart_item.quantity -= 1  # then decrease the quantity by 1
+            cart_item.save()  # save the cart_item
+        else:
+            cart_item.delete()  # else delete the cart_item
+    except:
+        pass
     return redirect('cart')
 
 
 def remove_cart_item(request,
-                     product_id):  # remove_cart_item() is used to remove the cart_item from the cart for (x) button
+                     product_id,cart_item_id):  # remove_cart_item() is used to remove the cart_item from the cart for (x) button
     cart = Cart.objects.get(cart_id=_cart_id(request))  # get the cart using the cart_id present in the session
     product = get_object_or_404(Product, id=product_id)  # get the product
-    cart_item = CartItem.objects.get(product=product, cart=cart)  # get the cart_item using the product and cart
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)  # get the cart_item using the product and cart
     cart_item.delete()  # delete the cart_item
     return redirect('cart')
 
